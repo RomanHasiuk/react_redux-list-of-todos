d720183b40408a3e566cd433c2a06f5f1efdfac4
RomanHasiuk <romanhasiuk1@gmail.com>
2025-09-19 23:33:33 +0300

    added to App.tsx import React

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
